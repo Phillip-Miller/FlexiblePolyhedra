c5437453cbdfe1ac8e5254721a0244c1272356d4
Phillip Miller <39445692+Phillip-Miller@users.noreply.github.com>
2021-06-11 12:00:04 -0700

    Switched from planes to shapes with depth
    Planes were causing rendering issues in unity (only renders one side)
    3D shapes must have correct vertex order too.

--- steffansTriangles.py
+++ steffansTriangles.py
@@ -2,17 +2,24 @@ import bpy
 from math import *
 #@Author Phillip M 6/9/2021
 #Created blender script to make precise triangles given coordinates to allow for custom angles and whatnot
-
-verts = [[0,0,0], [17,0,0], [12*cos(0.783662049 ),12*sin(0.783662049),0]]
-#verts = [[0,0,0], [10,0,0], [10*cos(0.0.988432089 ),10*sin(0.0.988432089),0]]
+width = .5
+#Triangles Labled A,B,C,D Respectively
+#verts = [[0,0,0], [17,0,0], [12*cos(0.783662049 ),12*sin(0.783662049),0]]
+#verts = [[0,0,0], [10,0,0], [10*cos(0.988432089),10*sin(0.988432089),0]]
 #verts = [[0,0,0], [11,0,0], [12*cos(1.09467727),-1*12.0*sin(1.09467727),0]]
-#verts = [[0,0,0], [12,0,0], [5*cos(0.958192179),5*sin(0.958192179),0]]
+verts = [[0,0,0], [12,0,0], [5*cos(0.958192179),5*sin(0.958192179),0]]
+
+for x in range(3): #Reusable way of changing width if needed
+    addWidth = verts[x].copy()
+    addWidth[2]+= width
+    verts.append(addWidth)
 edges = []
-faces = []
-#             x  y z
+
+faces = [[0,1,2],[3,4,5],[0,1,4,3],[1,2,5,4],[0,2,5,3]]
+#Need to make sure you go clockwise/counter clock wise for shapes with more than 3 verticies
 
 
-faces.append([0,1,2])
+
 
 #mod_skin = obj.modifiers.new('Skin','SKIN')
 
