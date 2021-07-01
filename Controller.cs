f9c23a86f5ae6ae6d18fd9f8f6f1096baf84fd9d
Phillip Miller <39445692+Phillip-Miller@users.noreply.github.com>
2021-07-01 15:57:00 -0700

    Update Controller.cs

--- Controller.cs
+++ Controller.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Script to enable player to move the camera around during the game scene
-public class CameraMovement : MonoBehaviour
+/*
+ * Enables user input
+ */
+public class Controller : MonoBehaviour
 {
     public float movementSpeed;
     public float rotationSpeed;
+    public float forceMultiplier;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +21,59 @@ public class CameraMovement : MonoBehaviour
     Shift : Increase speed
     Space : Moves camera up per its local Y-axis
     Cntrl : Moves camera down per its local Y-axis
+    left click = apply force push to normal on face
+    shift left click = apply force pull to normal on face 
+    right click is stop face from moving
+
     */
+    
      
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 c_Velocity = GetBaseMovement()*movementSpeed * Time.deltaTime;
+        Vector3 c_Velocity = GetBaseMovement() * movementSpeed * Time.deltaTime;
         Vector3 c_Rotation = GetBaseRotation() * rotationSpeed * Time.deltaTime;
         transform.Translate(c_Velocity);
         transform.Rotate(c_Rotation);
+        if (Input.GetMouseButton(0)) //not clear if normal calculated is correct
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                var go = hit.collider.gameObject;
+                Debug.Log("Left click" + go.name);
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+
+                    go.GetComponent<Rigidbody>().AddForce(1 * hit.normal * forceMultiplier);
+                    print("SHIFT" + -1 * hit.normal * forceMultiplier);
+                }
+                else
+                {
+                    go.GetComponent<Rigidbody>().AddForce(-1 * hit.normal * forceMultiplier);
+                    print("NOSHIFT" + -1 * hit.normal * forceMultiplier);
+
+                }
+            }
+        }
+        if (Input.GetMouseButton(1))
+        { //right click
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var go = hit.collider.gameObject;
+                Debug.Log("Right click" + go.name);
+                var Rb = go.GetComponent<Rigidbody>();
+                Rb.angularVelocity= Vector3.zero;
+                Rb.velocity = Vector3.zero;
+
+            }
+        }
     }
     Vector3 GetBaseRotation()
     {
